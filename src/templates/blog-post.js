7c5f116149b27e5134547d0798e0710bfcd8243a
blazerius54 <79137837220@yandex.ru>
2021-01-25 21:46:46 +0300

    develop/some minor style fixes

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,15 +9,17 @@ const BlogPost = ({ data }) => (
             <h2 className="postTitle">
                 {data.markdownRemark.frontmatter.title}
             </h2>
-            <h4 className="author">
-                Автор: {data.markdownRemark.frontmatter.author}
-            </h4>
+            <div className="originHeaderWrapper">
+                <h4 className="author">
+                    Автор: {data.markdownRemark.frontmatter.author}
+                </h4>
+                <h5>
+                    <a href={data.markdownRemark.frontmatter.origin} target="_blank">
+                        Оригинал статьи
+                    </a>
+                </h5>
+            </div>
             <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-            <h5>
-                <a href={data.markdownRemark.frontmatter.origin} target="_blank">
-                    Оригинал статьи
-                </a>
-            </h5>
         </div>
     </Layout>
 );
