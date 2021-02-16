b06aa2b7d6fe2a56998df787297c70cfde3f76c7
blazerius54 <79137837220@yandex.ru>
2021-02-16 23:24:03 +0300

    develop/add main title

--- src/components/CircleBackground/index.js
+++ src/components/CircleBackground/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.scss';
+import { MainTitle } from '../MainTitle';
 
 const CircleBackground = () => (
     <div className="circleBackground">
         <div className="bigCircle" />
         <div className="smallCircle" />
+        <MainTitle />
     </div>
 );
 
---
+++ src/components/MainTitle/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.scss';
+
+export const MainTitle = () => (
+    <div className="mainTitleWrapper">
+        <h1>
+            Frontend blog
+        </h1>
+        <span>Переводы, статьи</span>
+    </div>
+);
